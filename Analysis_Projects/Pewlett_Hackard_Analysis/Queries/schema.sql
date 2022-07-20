56ff95fffdd04aa7832db4422150fae628300c23
Bethany Murillo <luckyb777@me.com>
2022-07-20 09:43:33 -0700

    updated schema.sql and retirement_info.csv added

--- Analysis_Projects/Pewlett_Hackard_Analysis/Queries/schema.sql
+++ Analysis_Projects/Pewlett_Hackard_Analysis/Queries/schema.sql
@@ -48,3 +48,25 @@ CREATE TABLE titles(
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 SELECT * FROM titles;
+
+-- RETIREMENT ELIGIBILITY
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- NUMBER OF EMPLOYEES RETIRING 
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- TELL POSTGRES TO SAVE DATA INTO TABLE NAMED "RETIREMENT_INFO"
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- confirm table created and see what looks like 
+SELECT * FROM retirement_info;
